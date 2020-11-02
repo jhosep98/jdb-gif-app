e4e49e021caafe31702862414617e8fd55379858
jhosep98 <jhosep149@hotmail.com>
2020-11-02 11:48:00 -0300

    adding button that adds more categories to the list

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const GifExpertApp = () => {
+  const [categories, setCategories] = useState([
+    "One Punch",
+    "Naruto",
+    "One Piece",
+  ]);
+  const handleAdd = () => {
+    setCategories((cats) => [...cats, "hunterxhunter"]);
+  };
   return (
     <div>
       <h2>GifExpertApp</h2>
       <hr />
+      <button onClick={handleAdd}>Add</button>
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </div>
   );
 };
